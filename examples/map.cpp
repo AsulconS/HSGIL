b5fe4d8389899565290c80a362fcbb7ab424f3ac
Adrian Bedregal <rolandobedregal321@gmail.com>
2022-01-30 23:18:38 -0500

    Tweaks

--- examples/map.cpp
+++ examples/map.cpp
@@ -3,8 +3,31 @@
 
 #include <iostream>
 
+template <typename T>
+void value(T&)
+{
+    std::cout << "lvalue" << std::endl;
+}
+
+template <typename T>
+void value(T&&)
+{
+    std::cout << "rvalue" << std::endl;
+}
+
+template <typename T>
+void foo(T&& arg)
+{
+    value(gil::hsgil_forward<T>(arg));
+}
+
 int main()
 {
+    int x;
+    foo(x);
+    foo(std::move(x));
+    foo(gil::hsgil_move(x));
+    foo(4);
 
     return 0;
 }
